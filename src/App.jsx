4cf2ed0b6a68e06329777810a142ff37f5678577
aryabilal <aryaarchilles@gmail.com>
2025-11-24 15:43:20 +0700

    Improvisasi

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,107 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function App() {
-    return (
-        <div className="app">
-            <Board />
-        </div>
-    )
+  return (
+    <div className="app">
+      <Board />
+    </div>
+  );
 }
 
-function Square({value, onSquareClick}) {
-    return (
-        <button className="square" onClick={onSquareClick}>
-            {value}
-        </button>
-    )
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 function Board() {
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [turn, setTurn] = useState('X');
-    const winner = calculateWinner(squares);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState("X");
+  const [message, setMessage] = useState();
+  const winner = calculateWinner(squares);
 
-    function handleTurn() {
-        const currentPlayer = turn;
-        setTurn(turn === 'X' ? 'O' : 'X');
-        return currentPlayer;
+  function handleTurn() {
+    const currentPlayer = turn;
+    setTurn(turn === "X" ? "O" : "X");
+    return currentPlayer;
+  }
+
+  function handleClick(i) {
+    if (squares[i] || winner) return;
+
+    const newSquares = squares.slice();
+    newSquares[i] = handleTurn();
+    setSquares(newSquares);
+
+    if (calculateWinner(newSquares)) {
+      setMessage("Resetting in 5 seconds...");
+
+      setTimeout(() => {
+        setSquares(Array(9).fill(null));
+        setTurn("X");
+        setMessage(null);
+      }, 5000);
+    } else if (checkBoard(newSquares)) {
+      setMessage("Board full! Resetting in 5 seconds");
+      
+      setTimeout(() => {
+        setSquares(Array(9).fill(null));
+        setMessage(null);
+      }, 5000);
     }
-    
-    function handleClick(i) {
-        if (squares[i] || winner) return;
+  }
 
-        const newSquares = squares.slice();
-        newSquares[i] = handleTurn();
-        setSquares(newSquares);
+  return (
+    <div className="boardContainer">
+      <div className="board">
+        {squares.map((square, i) => (
+          <Square 
+            key={i}
+            value={square}
+            onSquareClick={() => handleClick(i)}
+          />
+        ))}
+      </div>
 
-        if (calculateWinner(newSquares)) {
-            setTimeout(() => {
-                setSquares(Array(9).fill(null));
-                setTurn('X');
-            }, 5000);
-        } else if (checkBoard(newSquares)) {
-            setTimeout(() => {
-                setSquares(Array(9).fill(null));
-            }, 5000);
-        }
-    }
-
-    return (
-        <div className="board"> 
-            {squares.map((square, i) => {
-                return <Square key={i} value={square} onSquareClick={() => handleClick(i)}/>
-            })}
-            <h2 id="winText" style={{ display: winner ? "block" : "none" }}>We Have A Winner! And That is {winner}</h2>
-        </div>
-    )
+      <div className="info">
+        <h2
+          id="winText"
+          style={{ display: winner ? "flex" : "none" }}
+        >
+          We Have A Winner! And That is {winner}
+        </h2>
+        {message && (
+          <p>{message}</p>
+        )}
+        <span>Turn: {turn}</span>
+      </div>
+    </div>
+  );
 }
 
 function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
 
-    for (let [a, b, c] of lines) {
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
+  for (let [a, b, c] of lines) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
+  }
 
-    return null;
+  return null;
 }
 
 function checkBoard(squares) {
-    return squares.every((mark) => mark != null);
+  return squares.every((mark) => mark != null);
 }
